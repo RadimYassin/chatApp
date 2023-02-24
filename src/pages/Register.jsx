8a516e1c45f6f671af279d9882eb369c450a50b1
RadimYassin <radimyassin20@gamil.com>
2023-02-24 23:14:26 +0100

    create register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import {  createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase.js'
 const Register = () => {
+
+  const handlsubmit=async(e)=>{
+        e.preventDefault()
+        const dispalyname=e.target[0].value
+        const email=e.target[1].value
+        const password=e.target[2].value
+        const avatar=e.target[3].files[0]
+try {
+  const res=await createUserWithEmailAndPassword(auth, email, password)
+  
+} catch (error) {
+  console.log(error);
+}
+
+  
+  }
   return (
     <div className='Container'>
            <div className='Containerform'>
                   <h1>Register</h1>
-                  <form  className='form'>
+                  <form onSubmit={handlsubmit}  className='form'>
                       <input type="text" placeholder='name' />
                       <input type="text" placeholder='email' />
                       <input type="password"  placeholder='password' />
