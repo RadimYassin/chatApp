87eff133ab8b93a97e490b54c28680ad6ffcfa0c
RadimYassin <radimyassin20@gamil.com>
2023-02-25 22:31:29 +0100

    some changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getStorage, ref } from "firebase/storage";
-import { Firestore, getFirestore } from "firebase/firestore";
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDHx_1ecI_mSen_gFvLFLz49tj7yXoFEP4",
   authDomain: "chatapp-602b7.firebaseapp.com",
@@ -16,9 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-export const auth=getAuth()
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
 export const storage = getStorage();
-export const db = Firestore()
+export const db = getFirestore()
